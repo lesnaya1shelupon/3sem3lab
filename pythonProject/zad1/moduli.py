a34978400f53d9276081f7b6e0bb18cefdb07382
lesnaya1shelupon <102218456+lesnaya1shelupon@users.noreply.github.com>
2022-11-14 16:52:30 +0300

    Update moduli.py

--- pythonProject/zad1/moduli.py
+++ pythonProject/zad1/moduli.py
@@ -1,11 +1,8 @@
 if __name__ == '__main__':
-
-
     def say_name(name, surname):
 
 
         def sname():
             print("Уважаемый " + surname + 
             " " + name + "! Вы делаете работу по замыканиям функций.")
-
         return sname
