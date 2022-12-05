5d30496ee9f2b41ff08be980b45ecf50130f7870
lesnaya1shelupon <102218456+lesnaya1shelupon@users.noreply.github.com>
2022-12-05 10:21:37 +0300

    Update main.py

--- pythonProject/zad2/main.py
+++ pythonProject/zad2/main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env  python3
+# !/usr/bin/env  python3
 
 import sys
 from package import table, table_name, table_nam
@@ -11,11 +11,10 @@ if __name__ == '__main__':
     while True:
         command = input(">>> ").lower()
 
-        
         if command == 'exit':
             break
 
-            
+
         elif command == 'add':
             surname = input("Фамилия: ")
             name = input("Имя: ")
@@ -27,26 +26,26 @@ if __name__ == '__main__':
                 'name': name,
                 'post': post,
                 'data': datas,
-                }
+            }
             spisok.append(spisok_new)
 
             if len(spisok) > 1:
                 spisok.sort(key=lambda item: item.get('data', ''))
 
-                
+
         elif command == 'list':
             print(table())
             print(table_name())
             print(table())
-            
+
             for item_x in table_nam(spisok):
-                print(item_x)   
+                print(item_x)
             print(table())
-            
-            
+
+
         elif command == 'find':
             find = input("Введите знак Зодиака: ")
-            
+
             for find_item in spisok:
                 if find == find_item['post']:
                     new_spisok.append(find_item)
@@ -55,15 +54,15 @@ if __name__ == '__main__':
                 print(table())
                 print(table_name())
                 print(table())
-                
+
                 for item_qe in table_nam(new_spisok):
                     print(item_qe)
                 print(table())
-                
+
             else:
                 print('Таких пользователей не найдено', file=sys.stderr)
-             
-            
+
+
         elif command == 'help':
             print('Список команд:\n')
             print('add - добавить пользователя.')
@@ -71,8 +70,8 @@ if __name__ == '__main__':
             print('find <Знак зодиака> - запросить пользователей по знаку Зодиака.')
             print('help - Справочник.')
             print('exit - Завершить пработу программы.')
-            
-            
+
+
         else:
             print(f'Команда <{command}> не существует.', file=sys.stderr)
             print('Введите <help> для просмотра доступных команд')
