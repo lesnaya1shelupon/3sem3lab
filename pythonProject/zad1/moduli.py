56b0bede236a3679acd3731c622ac393a486132f
lesnaya1shelupon <102218456+lesnaya1shelupon@users.noreply.github.com>
2022-12-05 10:20:18 +0300

    Update moduli.py

--- pythonProject/zad1/moduli.py
+++ pythonProject/zad1/moduli.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 
 def say_name(name, surname):
-
-    
     def sname():
-        print("Уважаемый " + surname + 
-        " " + name + "! Вы делаете работу по замыканиям функций.")
+        print("Уважаемый " + surname +
+              " " + name + "! Вы делаете работу по замыканиям функций.")
+
     return sname
